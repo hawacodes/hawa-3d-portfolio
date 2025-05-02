628d01ad678087e67008ea95aac8438ae91756e8
Havva Beyza OZLUOGLU <havva.ozluoglu@siemens.com>
2025-05-02 08:58:26 +0300

    fix: language is not defined properly after refresh in the store

--- src/i18n.js
+++ src/i18n.js
@@ -1,22 +1,21 @@
-// filepath: /c:/Users/z004h8zv/Projects/hawa-3d-portfolio/src/i18n.js
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
-import { store } from './redux/store';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+import { store } from "./redux/store";
 
 i18n
   .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
+    fallbackLng: "en",
     debug: true,
     interpolation: {
       escapeValue: false,
     },
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
     },
   });
 
